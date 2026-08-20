0e5d70ab8852a379ebe64c273c2c2163a25be7fd
Gabby <murkymarshmallow@gmail.com>
2026-08-20 14:23:48 -0600

    MongoDB Connected

---
+++ app.js
@@ -0,0 +1,13 @@
+require("dotenv").config();
+const express = require("express");
+const app = express();
+const PORT = 4000;
+
+const connectDB = require("./server/config/db");
+connectDB();
+
+app.get("/", (req, res) => {
+	res.send("Hello World!");
+});
+
+app.listen(PORT, () => console.log(`Server is running on port:${PORT}`));
