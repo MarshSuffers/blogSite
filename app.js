9a8becdad7f460a14fc499750ecbbaa443fe2d74
Gabby <murkymarshmallow@gmail.com>
2026-08-27 09:01:55 -0600

    finished assignment 1
    works so far, loads up hello world

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 require("dotenv").config();
+
+const expressLayouts = require("express-ejs-layouts");
 const express = require("express");
 const app = express();
 const PORT = 4000;
@@ -6,8 +8,14 @@ const PORT = 4000;
 const connectDB = require("./server/config/db");
 connectDB();
 
-app.get("/", (req, res) => {
-	res.send("Hello World!");
-});
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(expressLayouts);
+app.use(express.static("public"));
+app.set("layout", "./layouts/main");
+app.set("view engine", "ejs");
+
+app.use("/", require("./server/routes/main"));
 
 app.listen(PORT, () => console.log(`Server is running on port:${PORT}`));
--- server/config/db.js
+++ server/config/db.js
@@ -5,12 +5,28 @@ const dbPath = process.env.MONGO_URI;
 const connectDB = async () => {
 	try {
 		await mongoose.connect(dbPath);
-        console.log("MongoDB Connected");
-        
+		console.log("MongoDB Connected");
 	} catch (error) {
 		console.log(error);
 		process.exit(1);
 	}
 };
+
+const Post = require("../models/Post");
+
+function insertPostData() {
+	Post.insertMany([
+		{
+			title: "Post One",
+			body: "This is the first post we will be adding.",
+		},
+		{
+			title: "Post Two",
+			body: "This is another post we will be adding.",
+		},
+	]);
+}
+
+insertPostData();
 
 module.exports = connectDB;
