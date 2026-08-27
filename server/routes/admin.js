ee8e860aebda06ad0452063d2116f7938f194968
Gabby <murkymarshmallow@gmail.com>
2026-08-27 15:52:19 -0600

    Needs Testing
    Theoretically finished, just need to test functionality

--- server/routes/admin.js
+++ server/routes/admin.js
@@ -7,6 +7,21 @@ const jwt = require("jsonwebtoken");
 const jwtSecret = process.env.JWT_SECRET;
 const adminLayout = "../views/layouts/admin";
 
+const authMiddleware = (req, res, next) => {
+	const token = req.cookies.token;
+	if (!token) {
+		return res.status(401).json({ message: "Unathorized" });
+	}
+
+	try {
+		const decoded = jwt.verify(token, jwtSecret);
+		req.userId = decoded.userId;
+		next();
+	} catch (error) {
+		return res.status(401).json({ message: "Unathorized" });
+	}
+};
+
 router.get("/admin", async (req, res) => {
 	try {
 		const locals = {
@@ -15,6 +30,76 @@ router.get("/admin", async (req, res) => {
 		};
 
 		res.render("admin/index", { locals, layout: adminLayout });
+	} catch (error) {
+		console.log(error);
+	}
+});
+
+router.post("/admin", async (req, res) => {
+	try {
+		const { username, password } = req.body;
+
+		const user = await User.findOne({ username });
+
+		if (!user) {
+			return res.status(401).json({ message: "Invalid Credentials" });
+		}
+
+		const isPasswordValid = await bcrypt.compare(password, user.password);
+
+		if (!isPasswordValid) {
+			return res.status(401).json({ message: "Invalid Credentials" });
+		}
+
+		const token = jwt.sign({ userId: user._id }, jwtSecret);
+		res.cookie("token", token, { httpOnly: true });
+		res.redirect("/dashboard");
+	} catch (error) {
+		console.log(error);
+	}
+});
+
+router.get("/dashboard", authMiddleware, async (req, res) => {
+	try {
+		const locals = {
+			title: Dashboard,
+			description: "A blog template made with NodeJS and ExpressJS",
+		};
+
+		const data = await Post.find();
+		res.render("admin/dashboard", { locals, data, layout: adminLayout });
+	} catch (error) {}
+});
+
+router.post("/register", async (req, res) => {
+	try {
+		const { username, password } = req.body;
+		const hashedPassword = await bcrypt.hash(password, 10);
+
+		try {
+			const user = await User.create({
+				username,
+				password: hashedPassword,
+			});
+			res.status(201).json({ message: "User created successfully", user });
+		} catch (error) {
+			if (error === 11000) {
+				return res.status(500).json({ message: "User already Exists!" });
+			} else {
+				return res
+					.status(500)
+					.json({ message: "Something went wrong with the server!" });
+			}
+		}
+	} catch (error) {
+		console.log(error);
+	}
+});
+
+router.get("/logout", authMiddleware, async (req, res) => {
+	try {
+		res.clearCookie("token");
+		res.redirect("/");
 	} catch (error) {
 		console.log(error);
 	}
